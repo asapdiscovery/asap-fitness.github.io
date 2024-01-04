50282d5977eadd0608921573c510404e104315cf
Will Hannon <hannonww@gmail.com>
2024-01-04 11:07:19 -0800

    Add a custom component for contributors

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,17 +1,18 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
-import DefaultTheme from 'vitepress/theme'
-import './style.css'
+import { h } from "vue";
+import DefaultTheme from "vitepress/theme";
+import Contributors from "./Contributors.vue";
+import "./style.css";
 
 /** @type {import('vitepress').Theme} */
 export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
+      "home-features-after": () => h(Contributors),
+    });
   },
   enhanceApp({ app, router, siteData }) {
     // ...
-  }
-}
+  },
+};
