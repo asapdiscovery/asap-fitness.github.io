d5665d040b18b67e9fff43218e734c744a29ed91
Will Hannon <hannonww@gmail.com>
2024-01-05 10:44:20 -0800

    Filter posts based on directory

--- .vitepress/theme/BlogIndex.vue
+++ .vitepress/theme/BlogIndex.vue
@@ -2,6 +2,18 @@
 import { data as postsData } from './posts.data.js'
 
 export default {
+    computed: {
+        filteredPosts() {
+            // Extract the current directory name from the window location
+            const currentDirectory = window.location.pathname.split('/').slice(-2, -1)[0];
+
+            return postsData.filter(post => {
+                // Extract the parent directory name from the post URL
+                const parentDirectory = new URL(post.url, location.href).pathname.split('/').slice(-3, -2)[0];
+                return parentDirectory === currentDirectory;
+            });
+        }
+    },
     data() {
         return {
             posts: postsData
@@ -13,7 +25,7 @@ export default {
 <template>
     <h2>Experiment Index</h2>
     <ul>
-        <li v-for="post in posts" :key="post.id">
+        <li v-for="post in filteredPosts" :key="post.id">
             <a :href="post.url">{{ post.frontmatter.title }}</a>
         </li>
     </ul>
