3007350bbdc5b43c506eaadbbbd061dd97d17ed8
Will Hannon <hannonww@gmail.com>
2024-01-09 11:27:06 -0800

    Remove unused date formatter

--- .vitepress/theme/posts.data.js
+++ .vitepress/theme/posts.data.js
@@ -15,16 +15,3 @@ export default createContentLoader("./**/posts/*.md", {
       .sort((a, b) => new Date(b.date) - new Date(a.date)); // Sort by date assuming date is in a standard format
   },
 });
-
-function formatDate(raw) {
-  const date = new Date(raw);
-  date.setUTCHours(12);
-  return {
-    time: +date,
-    string: date.toLocaleDateString("en-US", {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    }),
-  };
-}
