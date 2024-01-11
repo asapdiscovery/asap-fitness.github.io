6ee68aa20b37ebba10f1b28201fa06574f2ec482
Will Hannon <hannonww@gmail.com>
2024-01-11 13:02:33 -0800

    Remove redundant ref in template

--- .vitepress/theme/Altair.vue
+++ .vitepress/theme/Altair.vue
@@ -59,8 +59,7 @@ export default {
 </script>
 
 <template>
-    <div class="vega-chart-container" ref="vegaContainer">
-        <div ref="vegaContainer" style="width: 100%; height: 100%;"></div>
+    <div class="vega-chart-container" ref="vegaContainer" style="width: 100%; height: 100%;">
     </div>
 </template>
 
